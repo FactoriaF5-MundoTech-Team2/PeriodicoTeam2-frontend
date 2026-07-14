6cf2655906aa5aabff965b349d9c15067b5387ab
nagicome03 <nayeligiselle.0206@gmail.com>
2026-07-14 10:00:57 +0200

    App.jsx refactorized

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-//import Header from "./components/Header/Header";
-//import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router";
-//import CardImage from "./components/CardImage/CardImage";
-//import PublishButton from "./features/articles/components/PublishButton/PublishButton";
-//import SearchBar from "./features/articles/components/SearchBar/SearchBar";
-//import Footer from "./components/Footer/Footer";
-import FilterButton from "./features/articles/components/FilterButton/FilterButton";
 
 function App() {
 
@@ -13,12 +6,7 @@ function App() {
     <>
       <main>
         <Outlet />
-        {/* <PublishButton/> */}
-        {/* <SearchBar/> */}
-        <FilterButton/>
-        {/* <CardImage/> */}
       </main>
-      {/* <Footer/> */}
     </>
   )
 }
