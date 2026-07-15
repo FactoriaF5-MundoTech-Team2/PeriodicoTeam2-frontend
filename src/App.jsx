8ed90bd11691c8a8d1664572527017b2165b25ac
Kressala <chdimaio@protonmail.com>
2026-07-15 11:55:52 +0200

    App changed to include AppRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-<<<<<<< HEAD
-//import Header from "./components/Header/Header";
-//import Footer from "./components/Footer/Footer";
-import { Outlet } from "react-router";
-
-function App() {
-
-  return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  )
-}
-=======
 import AppRoutes from "./routes/AppRoutes"
 
 function App() {
-  return <AppRoutes />
+return <AppRoutes />
 }
 
->>>>>>> 89fe3162514d53d70169d8528cdd1d6f622dad2f
 export default App
