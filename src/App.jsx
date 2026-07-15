fa6baf94fc8431c51450adfa5eef0f2170f73986
Johanna Monroy <johamonroy20@gmail.com>
2026-07-15 11:56:53 +0200

    feat: add TagStatus component with published, in review and draft variants

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-//import Header from "./components/Header/Header";
-import Button from "./features/articles/components/RejectButton/RejectButton";
-import { Outlet } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes"
 
 function App() {
-  return (
-    <>
-      <main>
-        {/* <Header /> */}
-        <Button />
-        <Outlet />
-      </main>
-    </>
-  );
+  return <AppRoutes />
 }
-export default App;
+
+export default App
