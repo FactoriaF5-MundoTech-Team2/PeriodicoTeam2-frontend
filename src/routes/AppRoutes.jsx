de503d1e003779ba93658c3f79d03f27ef868d3b
nagicome03 <nayeligiselle.0206@gmail.com>
2026-07-15 13:09:16 +0200

    fix: imports AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
-import App from '../App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AuthorHome from '../features/articles/pages/AuthorHome'
 import ManagerHome from '../features/articles/pages/ManagerHome'
 import CreateArticle from '../features/articles/pages/CreateArticle'
